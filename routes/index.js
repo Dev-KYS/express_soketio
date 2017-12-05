f783b6f99421aef37b3ed9eb3b07897fa77c0410
김용식 <dydtlr19@gmail.com>
2017-12-05 17:20:05 +0900

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'kim147258',
+    port: 3306,
+    database: 'preorder'
+});
+
+conn.connect();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var data = '';
+    conn.query("SELECT * FROM test_board", function (err, rows, fields) {
+        if(!err){
+
+            data = JSON.stringify(rows);
+            console.log(data);
+            res.render('index', { title: 'Express', boards: rows });
+        }
+    });
+
+});
+
+module.exports = router;
